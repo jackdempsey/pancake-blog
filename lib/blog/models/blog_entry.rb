71813bd0e22fb88334f73c9c4db725c5a65d61e7
Daniel Neighman <has.sox@gmail.com>
2009-11-14 22:40:30 +1100

    Updates the namespace for the model and sets it to be an inheritable inner class

--- lib/blog/models/blog_entry.rb
+++ lib/blog/models/blog_entry.rb
@@ -1,5 +1,9 @@
-class BlogEntry < ActiveRecord::Base
-  def to_s
-    title
+class Blog
+  inheritable_inner_classes :BlogEntry
+
+  class BlogEntry < ActiveRecord::Base
+    def to_s
+      title
+    end
   end
 end
