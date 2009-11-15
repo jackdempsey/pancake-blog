c13f1564f66940251ac74efe5cd128d90985e2a9
Daniel Neighman <has.sox@gmail.com>
2009-11-15 14:30:21 +1100

    Removes a lot of guff from the config.rb file

--- lib/blog/config/config.rb
+++ lib/blog/config/config.rb
@@ -5,20 +5,6 @@ class Blog
   # Labels can be set in the config/environments/<env>.rb file to limit
   # middleware loading.
   # stack(:middleware_name, :labels => [:development, :production]).use(MiddlewareClass)
-
-  class self::Configuration
-    default :blog_title, "Pancake Blog", "A simple title for the blog"
-    # Add defaults to your stack configuration.
-    # This is scoped to this stack, and is inhertied into child stacks
-    #
-    # Fixed value defaults:
-    #   default :var_name, :value, "A description of the variable"
-    #
-    # Lazy Defaults:
-    #   default :var_name, lambda{ configuration_method }, "Some Description"
-
-    # Declare methods on your configuraiton
-    # def configuration_method; #stuff; end
-  end
+  self::Configuration.default :blog_title, "Pancake Blog", "A simple title for the blog"
 end
 
