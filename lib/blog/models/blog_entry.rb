633541abcaf033c97a043f418a01d16b40f43197
Daniel Neighman <has.sox@gmail.com>
2009-11-15 12:21:36 +1100

    Sorts STI and the form url

--- lib/blog/models/blog_entry.rb
+++ lib/blog/models/blog_entry.rb
@@ -2,6 +2,9 @@ class Blog
   inheritable_inner_classes :BlogEntry
 
   class BlogEntry < ActiveRecord::Base
+    # Without this we can't actually make use of sti :(
+    self.store_full_sti_class = true
+
     def to_s
       title
     end
